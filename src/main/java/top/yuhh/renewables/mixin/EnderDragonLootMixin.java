258a385548b2fc7cad3f424f4cee86ad8cd6537f
Mervius <dhagemeier2001@gmail.com>
2025-11-24 15:15:49 -0600

    Make Dragon head renewable... technically...

---
+++ src/main/java/top/yuhh/renewables/mixin/EnderDragonLootMixin.java
@@ -0,0 +1,25 @@
+package top.yuhh.renewables.mixin;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Mob.class)
+public class EnderDragonLootMixin {
+    @Inject(method = "dropCustomDeathLoot", at = @At("HEAD"), cancellable = true)
+    protected void dropCustomLoot(ServerLevel level, DamageSource damageSource, boolean recentlyHit, CallbackInfo ci) {
+        if ((Object) this.getClass() == EnderDragon.class) {
+            if (damageSource.getEntity() instanceof Creeper creeper && creeper.canDropMobsSkull()) {
+                creeper.increaseDroppedSkulls();
+                ((Mob) (Object) this).spawnAtLocation(Items.DRAGON_HEAD);
+            }
+        }
+    }
+}
