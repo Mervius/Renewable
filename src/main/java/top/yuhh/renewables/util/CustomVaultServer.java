b87c0e1093126a1403e144353b42421931e56494
Mervius <dhagemeier2001@gmail.com>
2025-11-26 16:04:24 -0600

    vaults reset at midnight of the new moon(technically 1 tick after midnight)

--- src/main/java/top/yuhh/renewables/util/CustomVaultServer.java
+++ src/main/java/top/yuhh/renewables/util/CustomVaultServer.java
@@ -11,8 +11,8 @@ import net.minecraft.world.level.block.state.BlockState;
 public final class CustomVaultServer {
     public static void customTick(ServerLevel level, BlockPos pos, BlockState state, VaultConfig config, VaultServerData serverData, CustomVaultServerData customServerData, VaultSharedData sharedData) {
         VaultBlockEntity.Server.tick(level, pos, state, config, serverData, sharedData);
-        long moon = (level.getMoonPhase() + 4) % 8;
-        long day = level.getDayTime() / 24000L;
+        long moon = ((level.getDayTime() - 18000) / 24000L % 8L + 4L) % 8;
+        long day = (level.getDayTime() - 18000) / 24000L;
         if ((day - moon) != customServerData.getLastResetTime()) {
             customServerData.setLastResetTime(level);
             ((resetPlayers)serverData).renewable$resetPlayersMethod();
