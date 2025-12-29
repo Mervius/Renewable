e707c3b11dd2d01ce3b487cce02bc614ffccedb3
Mervius <dhagemeier2001@gmail.com>
2025-12-29 13:14:11 -0600

    Add Map Color

--- src/main/java/top/yuhh/renewables/block/ModBlocks.java
+++ src/main/java/top/yuhh/renewables/block/ModBlocks.java
@@ -5,6 +5,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -16,35 +17,35 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Renewables.MOD_ID);
 
-    public static final DeferredBlock<Block> LACED_GRAPHITE = registerBlock("laced_graphite", () -> new LacedGraphiteBlock(UniformInt.of(0, 2), BlockBehaviour.Properties.of().sound(SoundType.TUFF).randomTicks().requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> LACED_GRAPHITE = registerBlock("laced_graphite", () -> new LacedGraphiteBlock(UniformInt.of(0, 2), BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).randomTicks().requiresCorrectToolForDrops().strength(4.5F,4F)));
 
-    public static final DeferredBlock<Block> RICH_GRAPHITE = registerBlock("rich_graphite", () -> new RichGraphiteBlock(UniformInt.of(0,2), BlockBehaviour.Properties.of().sound(SoundType.TUFF).randomTicks().requiresCorrectToolForDrops().strength(3.5F,3F)));
+    public static final DeferredBlock<Block> RICH_GRAPHITE = registerBlock("rich_graphite", () -> new RichGraphiteBlock(UniformInt.of(0,2), BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).randomTicks().requiresCorrectToolForDrops().strength(3.5F,3F)));
 
-    public static final DeferredBlock<Block> GRAPHITE_BLOCK = registerBlock("graphite_block", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> GRAPHITE_BLOCK = registerBlock("graphite_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
-    public static final DeferredBlock<Block> GRAPHITE_SLAB = registerBlock("graphite_slab", () -> new SlabBlock(BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> GRAPHITE_SLAB = registerBlock("graphite_slab", () -> new SlabBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
-    public static final DeferredBlock<Block> GRAPHITE_STAIRS = registerBlock("graphite_stairs", () -> new StairBlock(ModBlocks.GRAPHITE_BLOCK.get().defaultBlockState(), BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> GRAPHITE_STAIRS = registerBlock("graphite_stairs", () -> new StairBlock(ModBlocks.GRAPHITE_BLOCK.get().defaultBlockState(), BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
     public static final DeferredBlock<Block> GRAPHITE_WALL = registerBlock("graphite_wall", () -> new WallBlock(BlockBehaviour.Properties.ofLegacyCopy(GRAPHITE_BLOCK.get()).forceSolidOn()));
 
-    public static final DeferredBlock<Block> POLISHED_GRAPHITE = registerBlock("polished_graphite", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> POLISHED_GRAPHITE = registerBlock("polished_graphite", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
-    public static final DeferredBlock<Block> POLISHED_GRAPHITE_SLAB = registerBlock("polished_graphite_slab", () -> new SlabBlock(BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> POLISHED_GRAPHITE_SLAB = registerBlock("polished_graphite_slab", () -> new SlabBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
-    public static final DeferredBlock<Block> POLISHED_GRAPHITE_STAIRS = registerBlock("polished_graphite_stairs", () -> new StairBlock(ModBlocks.POLISHED_GRAPHITE.get().defaultBlockState(), BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> POLISHED_GRAPHITE_STAIRS = registerBlock("polished_graphite_stairs", () -> new StairBlock(ModBlocks.POLISHED_GRAPHITE.get().defaultBlockState(), BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
     public static final DeferredBlock<Block> POLISHED_GRAPHITE_WALL = registerBlock("polished_graphite_wall", () -> new WallBlock(BlockBehaviour.Properties.ofLegacyCopy(POLISHED_GRAPHITE.get()).forceSolidOn()));
 
-    public static final DeferredBlock<Block> GRAPHITE_BRICKS = registerBlock("graphite_bricks", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> GRAPHITE_BRICKS = registerBlock("graphite_bricks", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
-    public static final DeferredBlock<Block> GRAPHITE_BRICK_SLAB = registerBlock("graphite_brick_slab", () -> new SlabBlock(BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> GRAPHITE_BRICK_SLAB = registerBlock("graphite_brick_slab", () -> new SlabBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
-    public static final DeferredBlock<Block> GRAPHITE_BRICK_STAIRS = registerBlock("graphite_brick_stairs", () -> new StairBlock(ModBlocks.GRAPHITE_BLOCK.get().defaultBlockState(), BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> GRAPHITE_BRICK_STAIRS = registerBlock("graphite_brick_stairs", () -> new StairBlock(ModBlocks.GRAPHITE_BLOCK.get().defaultBlockState(), BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
     public static final DeferredBlock<Block> GRAPHITE_BRICK_WALL = registerBlock("graphite_brick_wall", () -> new WallBlock(BlockBehaviour.Properties.ofLegacyCopy(GRAPHITE_BLOCK.get()).forceSolidOn()));
 
-    public static final DeferredBlock<Block> CRACKED_GRAPHITE_BRICKS = registerBlock("cracked_graphite_bricks", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
+    public static final DeferredBlock<Block> CRACKED_GRAPHITE_BRICKS = registerBlock("cracked_graphite_bricks", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.TUFF).requiresCorrectToolForDrops().strength(4.5F,4F)));
 
 //    public static final DeferredBlock<Block> NETHERITE_CATALYST = registerBlock("netherite_catalyst", () -> new OreEnricherBlock(BlockBehaviour.Properties.of().randomTicks().requiresCorrectToolForDrops()));
 
